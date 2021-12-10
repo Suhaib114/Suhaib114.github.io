316f66ce1144e5cbaa6ef8439b03b23b1b344047
Suhaib Ahmed <42011643+Suhaib114@users.noreply.github.com>
2021-12-10 23:24:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["IT Analyst", "Tech Enthusiast ", "Developer"],
+        strings: ["IT Analyst", "Tech Enthusiast ", "Developer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["IT Analyst", "Tech Enthusiast ", "Developer"],
+        strings: ["IT Analyst", "Tech Enthusiast ", "Developer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
